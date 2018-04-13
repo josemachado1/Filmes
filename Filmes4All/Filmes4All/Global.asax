ba4f142537b232239c093e54ccbb1350705ef133
José <Jos@JOSE>
2018-04-13 21:39:34 +0100

    init da aplicaçao e classe Filmes

---
+++ Filmes4All/Filmes4All/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Filmes4All.MvcApplication" Language="C#" %>
---
+++ Filmes4All/Filmes4All/Models/Filmes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Filmes4All.Models
+{
+    public class Filmes
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Ano { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Capa { get; set; }
+
+        public double Preco { get; set; }
+    }
+}
